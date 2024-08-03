5c1a8fb1379b0e222a9f4fe6d99ea6d5ecfd229d
Aminul Islam <alfaz1873@gmail.com>
2024-08-03 18:27:21 +0530

    profile bug fix

--- routes/user.js
+++ routes/user.js
@@ -12,14 +12,14 @@ router.get("/login", (req,res)=>{return res.render("login")});
 router.get("/logout", (req,res)=>{
     return res.clearCookie("uid").redirect("/");
 });
-router.get("/profile", (req,res)=>{ 
+router.get("/profile", restrictToLoggedInUserOnly ,(req,res)=>{ 
 
     return res.render("userProfile", {
         name: req.user.name,
         email: req.user.email
     })
 });
-router.post("/profile", handlerUserProfile);
+router.post("/profile", restrictToLoggedInUserOnly ,handlerUserProfile);
 router.get('/create-order', (req,res)=>{return res.render('payment')});
 router.post('/create-order', handleTransaction);
 router.post('/update-user-access', updateUserAccess);
